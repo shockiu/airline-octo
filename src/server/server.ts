158bd2f625011466f523020eef7059084977c55a
shockiu <galeanocarlosd@gmail.com>
2023-05-07 14:25:59 -0400

    modificada consulta de vuelos con pases y pasajeros

--- src/server/server.ts
+++ src/server/server.ts
@@ -30,23 +30,7 @@ export class Server {
             // Morgan config
            // this.app.use(this.morganConfigMiddleware());
         }
-        /*
-        morganConfigMiddleware() {
-            return morgan((tokens: any, req: any, res: any)  => {
-                return [
-                    chalk.hex('#ff4757').bold('⚡ Morgan --> '),
-                    chalk.hex('#34ace0').bold(tokens.method(req, res)),
-                    chalk.hex('#ffb142').bold(tokens.status(req, res)),
-                    chalk.hex('#ff5252').bold(tokens.url(req, res)),
-                    chalk.hex('#2ed573').bold(tokens['response-time'](req, res) + ' ms'),
-                    chalk.hex('#f78fb3').bold('@ ' + tokens.date(req, res)),
-                    chalk.yellow(tokens['remote-addr'](req, res)),
-                    chalk.hex('#fffa65').bold('from ' + tokens.referrer(req, res)),
-                    chalk.hex('#1e90ff')(tokens['user-agent'](req, res)),
-                ].join(' ');
-            })
-        }
-        */
+      
         routes() {
            this.app.use( this.apiPath.routes,  routes );
         }
--- src/services/flightsService.ts
+++ src/services/flightsService.ts
@@ -8,8 +8,37 @@ export class FlightService {
     constructor(){}
 
     async findFlightsById(id: number | string) {
-        return await models.flight.findAll({
-            where: {flight_id: id}
+        return await models.flight.findOne({
+            where: {flight_id: id},
+            attributes: [
+                ['flight_id', 'flightId'], 
+                ['takeoff_date_time', 'takeoffDateTime'],
+                ['takeoff_airport', 'takeoffAirport'],
+                ['landing_date_time', 'landingDateTime'],
+                ['landing_airport', 'landingAirport'],
+                ['airplane_id', 'airplaneId']
+            ],
+            include:[
+                {
+                    model: models.boardingPass,
+                    as: 'boarding_passes',
+                    attributes: [
+                        ['passenger_id', 'passengerId'],
+                        ['boarding_pass_id', 'boardingPassId'],
+                        ['purchase_id', 'purchaseId'],
+                        ['seat_type_id', 'seatTypeId'],
+                        ['seat_id', 'seatId'],
+                        //[db.col('passenger.name'), 'name']
+                    ],
+                    include: [
+                        {
+                            model: models.passenger,
+                            as: 'passenger',
+                            //attributes: []
+                        }
+                    ]
+                }
+            ]
         });
     }
 }
